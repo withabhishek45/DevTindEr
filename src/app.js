f369956b1621dd0ffadf505e83a561d0f6f61070
withabhishek45 <abhishekkumar28102005@gmail.com>
2024-12-27 12:13:49 +0530

    Route Handler

--- src/app.js
+++ src/app.js
@@ -3,26 +3,36 @@ const express=require('express')
 const app=express();
 
 
-app.get("/user",(req,res)=>{
-    res.send({fname:"Abhishek",lname:"Kumar"})
-})
-app.post("/user",(req,res)=>{
-    res.send("Succesfully saved to Database....")
-})
-app.delete("/user",(req,res)=>{
-    res.send("Data deleted sucessfully.....")
-})
-app.use("/home",(req,res)=>{
-    res.send('Hello World')
-})
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the !st route user....");
+   res.send('Response!!!');
+   next();
+},
+    (req,res)=>{
+        console.log("Handling the 2nd route user....");
+        res.send(' Second Response!!!');
+    
+});
+// app.get("/user",(req,res)=>{
+//     res.send({fname:"Abhishek",lname:"Kumar"})
+// })
+// app.post("/user",(req,res)=>{
+//     res.send("Succesfully saved to Database....")
+// })
+// app.delete("/user",(req,res)=>{
+//     res.send("Data deleted sucessfully.....")
+// })
+// app.use("/home",(req,res)=>{
+//     res.send('Hello World')
+// })
 
-app.use("/login",(req,res)=>{
-    res.send('Login here')
-})
+// app.use("/login",(req,res)=>{
+//     res.send('Login here')
+// })
 
-app.use("/",(req,res)=>{
-    res.send('Welcome')
-})
+// app.use("/",(req,res)=>{
+//     res.send('Welcome')
+// })
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 });
