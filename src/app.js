1170d515f91786c20cac7ac95ab7566d7fb2695f
withabhishek45 <abhishekkumar28102005@gmail.com>
2024-12-29 15:54:33 +0530

    insert dynamic  json data through postman

--- src/app.js
+++ src/app.js
@@ -2,20 +2,28 @@ const express=require('express')
 const connectDB=require("./config/database")
 const USer=require("./models/user")
 const app=express();
+app.use(express.json(   ))
 
 app.post("/signup",async(req,res)=>{
+
+
+   console.log(req.body)
   
    //  creating new user model
-   const user=new USer({
-      name:"Abhishek kr",
-      email:"abhishek123@gmail.com",
-      password:"123456789",
-      phone:"015157",
-      address:"RAnchi jh",
-      role:"Student"
-   });
-   await user.save();
+   const user=new USer(req.body)
+   // const user=new USer({
+   //    name:"Abhishek kr",
+   //    email:"abhishek123@gmail.com",
+   //    password:"123456789",
+   //    phone:"015157",
+   //    address:"RAnchi jh",
+   //    role:"Student"
+   // });
+   try{   await user.save();
    res.send("User created successfully");
+   }catch(err){
+      res.status(400).send("User Data Not Saved"+err.message);
+      }
 })
    
 
