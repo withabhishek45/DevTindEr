72d705f63bdfc8ee3f35333dc0789ab450fc3c0e
withabhishek45 <abhishekkumar28102005@gmail.com>
2024-12-29 14:01:40 +0530

    2nd way of  error Handling using try catch

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 const express=require('express')
 
 const app=express();
-
-app.get("/getUserData",(req,res,next)=>{
-   console.log("get user data");
-
-   throw new console.error("Internal  Error");
-   res.send("Data send successfully...")
-   
-
-
-})
-
 app.use("/",(err,req,res,next)=>{
    if(err){
       //log your error
       res.status(400).send("Internal Server Error");
    }
 });
+
+app.get("/getUserData",(req,res,next)=>{
+   try{
+      
+   throw new Error("Internal  Error");
+   res.send("Data send successfully...")
+
+   }
+   catch(err){
+res.status(500).send("Something Went Wrong...  Contact Support Team ")
+
+   }
+
+   
+})
+
+
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 });
