02ca6607aeae3f9cd145cacaee0263974c65eddd
withabhishek45 <abhishekkumar28102005@gmail.com>
2024-12-29 15:11:31 +0530

    inserting data in DB

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express=require('express')
 const connectDB=require("./config/database")
+const USer=require("./models/user")
 const app=express();
 
-
+app.post("/signup",async(req,res)=>{
+  
+   //  creating new user model
+   const user=new USer({
+      name:"Abhishek kr",
+      email:"abhishek123@gmail.com",
+      password:"123456789",
+      phone:"015157",
+      address:"RAnchi jh",
+      role:"Student"
+   });
+   await user.save();
+   res.send("User created successfully");
+})
    
 
 connectDB()
